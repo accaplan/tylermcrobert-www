20260dff68fb76d3919d79d5f7e0792334a0a2b5
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2021-07-30 14:39:57 -0400

    Get data into page

--- src/pages/timeline.tsx
+++ src/pages/timeline.tsx
@@ -2,6 +2,7 @@ import { NextPage, GetStaticProps } from 'next'
 
 import { Layout } from 'components'
 import styled from 'styled-components'
+import { client } from 'lib/sanity'
 
 const TimelineWrapper = styled.section`
   background: black;
@@ -10,19 +11,32 @@ const TimelineWrapper = styled.section`
   left: 0;
   width: 100vw;
   height: 100vh;
+  color: white;
+  padding: 1.2rem;
+  padding-top: 4rem;
 `
 
-const Timeline: NextPage<{ data: any }> = ({}) => {
+const Timeline: NextPage<{
+  data: {
+    title?: string
+  }[]
+}> = ({ data }) => {
   return (
     <Layout title="Info">
-      <TimelineWrapper>timeline</TimelineWrapper>
+      <TimelineWrapper>
+        {data.map(timeline => (
+          <div key={timeline.title}>{timeline.title}</div>
+        ))}
+      </TimelineWrapper>
     </Layout>
   )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
+  const data = await client.fetch(`*[_type == 'timeline']`)
+
   return {
-    props: { data: '' },
+    props: { data },
   }
 }
 
