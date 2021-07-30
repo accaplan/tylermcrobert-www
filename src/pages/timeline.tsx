82b6fa71dcc6a97fc7f16ed0bf4e892fdc541b28
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2021-07-30 15:10:34 -0400

    layout changes

--- src/pages/timeline.tsx
+++ src/pages/timeline.tsx
@@ -4,6 +4,7 @@ import { Layout } from 'components'
 import styled from 'styled-components'
 import { client } from 'lib/sanity'
 import { useState } from 'react'
+import { size } from 'style'
 
 const TimelineWrapper = styled.section`
   background: black;
@@ -30,6 +31,63 @@ const Grid = styled.div`
   }
 `
 
+const MainGrid = styled.div`
+  display: flex;
+  font-size: ${size[2]};
+`
+
+const PowerTenStyle = styled.div`
+  position: absolute;
+  right: 0;
+  top: 50%;
+  border: 1px solid blue;
+  transform: translate3d(-50%, -50%, 0);
+
+  > div {
+    > :nth-child(1) {
+      padding-top: ${size[0]};
+    }
+    display: flex;
+    padding-top: ${size[0]};
+    transform: rotate(90deg);
+  }
+`
+const MetersStyle = styled.div`
+  transform-origin: top left;
+  border: 1px solid blue;
+  transform: translate3d(-50%, 50%, 0);
+  position: absolute;
+  padding-left: ${size[3]};
+
+  > div {
+    transform: rotate(-90deg);
+    width: 100vh;
+  }
+`
+
+const PowerTenIndicator: React.FC<{ number: number }> = () => {
+  return (
+    <PowerTenStyle>
+      <div>
+        <div>10</div>
+        <span>1</span>
+      </div>
+    </PowerTenStyle>
+  )
+}
+
+const SquareStyle = styled.div`
+  width: 100vw;
+  height: 100vh;
+  border: 1px solid blue;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
 const Timeline: NextPage<{
   data: {
     title?: string
@@ -46,6 +104,13 @@ const Timeline: NextPage<{
           <div>•</div>
           <div>{event.date}</div>
         </Grid>
+        <MainGrid>
+          <MetersStyle>
+            <div>1,000 meters</div>
+          </MetersStyle>
+          <SquareStyle>lorem ipsum dolor sit</SquareStyle>
+          <PowerTenIndicator number={0} />
+        </MainGrid>
       </TimelineWrapper>
     </Layout>
   )
@@ -58,5 +123,5 @@ export const getStaticProps: GetStaticProps = async () => {
     props: { data },
   }
 }
-
+;``
 export default Timeline
