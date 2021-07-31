e4a99a243499200cb9be0fb9d41f16fdac498cfc
Tyler McRobert <31973492+tylermcrobert@users.noreply.github.com>
2021-07-31 12:35:00 -0400

    add city

--- cms/schemas/timeline.js
+++ cms/schemas/timeline.js
@@ -8,14 +8,19 @@ export default {
       validation: Rule => Rule.required(),
     },
     {
-      name: 'date',
-      type: 'date',
+      name: 'dateTime',
+      type: 'datetime',
       validation: Rule => Rule.required(),
     },
     {
       name: 'show',
       type: 'boolean',
       title: 'Show date on timeline',
+      validation: Rule => Rule.required(),
+    },
+    {
+      name: 'city',
+      type: 'string',
       validation: Rule => Rule.required(),
     },
   ],
